8b27d2f629d1711d4faa43fa5d45c573b0948b38
sonuprajapati9794 <prajapati979473@gmail.com>
2026-02-12 12:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function daysOfAYear(year) {
+    if (year % 400 === 0) {
+        return 366;
+    } 
+    else if (year % 100 === 0) {
+        return 365;
+    } 
+    else if (year % 4 === 0) {
+        return 366;
+    } 
+    else {
+        return 365;
+    }
+}
 //your JS code here. If required.
